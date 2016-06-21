2410ff3e7a77d0cf9b5e964f55f3f1fede9b0ea4
Dennis Linsley <denlinsley@gmail.com>
2016-06-21 07:37:46 -0700

    Use .js file for eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
-{
+module.exports = {
   "extends": ["airbnb"],
   "parserOptions": {
     "ecmaFeatures": {
       "experimentalObjectRestSpread": true
-    },
+    }
+  },
+  "rules": {
+    semi: ["error", "never"]
   }
-}
+};
